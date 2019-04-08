ccb9e21e9a45e70e54bd7ecca9e7f81a2803d9ff
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-08 03:13:44 -0400

    math functions
    added gem  display + math.random

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
+var gem1 = Math.floor(Math.random() * 10);
+var gem2 = Math.floor(Math.random() * 10);
+var gem3 = Math.floor(Math.random() * 10);
+var gem4 = Math.floor(Math.random() * 10);
+var ranNum = Math.floor(Math.random() * 40);
 
+
+$(document).ready(function () {
+    $("#gem1").click(function () {
+        $("#gem1").html(gem1);
+    });
+    $("#gem2").click(function () {
+        $("#gem2").html(gem2);
+    });
+    $("#gem3").click(function () {
+        $("#gem3").html(gem3);
+    });
+    $("#gem4").click(function () {
+        $("#gem4").html(gem4);
+    });
+});
