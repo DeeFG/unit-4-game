3b362012b717a742297397b705265f4e4cd0e9d5
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-08 05:58:36 -0400

    added total score update

--- game.js
+++ game.js
@@ -3,25 +3,26 @@ var gem2 = Math.floor(Math.random() * (10 - 1) + 1 );
 var gem3 = Math.floor(Math.random() * (10 - 1) + 1 );
 var gem4 = Math.floor(Math.random() * (10 - 1) + 1 );
 var ranNum = Math.floor(Math.random() * (40 - 1) + 1 );
-clickScore = 0;
 totalScore =0;
 
 
 $(document).ready(function () {
+$("#ranNum").html(ranNum)
+
     $("#gem1").click(function () {
         $("#score").html(totalScore)
-        // diplay score
         $("#gem1").html(gem1);
         if (gem1 >=1 )
         totalScore ++ ;
         console.log("HH");
+        
         
     });
 
     $("#gem2").click(function () {
         $("#score").html(totalScore)
         $("#gem2").html(gem2);
-        if (gem1 >=2 )
+        if (gem1 >=1 )
         totalScore ++ ;
         console.log("EEE");
     });
