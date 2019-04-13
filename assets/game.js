b283e9153c3916261a9d52cf79a6bc95471b419e
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-12 23:06:17 -0400

    ADDED RANbOX RESET

--- assets/game.js
+++ assets/game.js
@@ -8,81 +8,40 @@ var win = 0;
 var loss = 0;
 
 
-
-
-var elementToClick = document.querySelector(".GEMS");
-var elementToShow = document.querySelector(".element");
-
-if(elementToClick){
- 	elementToClick.addEventListener("click",showElement)
-}
-
-function showElement(){
-	elementToShow.classList.add("show");
-}
-
-
-
-
-
 function reset() {
-
-    gem1 = Math.floor(Math.random() * (10 - 1) + 1);
-    gem2 = Math.floor(Math.random() * (10 - 1) + 1);
-    gem3 = Math.floor(Math.random() * (10 - 1) + 1);
-    gem4 = Math.floor(Math.random() * (10 - 1) + 1);
-    ranNum = Math.floor(Math.random() * (40 - 1) + 1);
+    var gem1 = Math.floor(Math.random() * (12 - 1) + 1);
+    var gem2 = Math.floor(Math.random() * (12 - 1) + 1);
+    var gem3 = Math.floor(Math.random() * (12 - 1) + 1);
+    var gem4 = Math.floor(Math.random() * (12 - 1) + 1);
+    var ranNum = Math.floor(Math.random() * (120 - 19) + 19);
     totalScore = 0;
-    // $(".GEMS").click(function() {
-    //     $('img').attr("src", "");
-    // });
-
-    $(".GEMS").click(function(){
-        $("img").attr("src", "");
-      });
-
-$("#ranNum").html(ranNum)
+    $("#ranNum").html(ranNum);
     $("#score").text(totalScore);
-   // showElement();
-    //$("img").attr("src")
-
-    $("src").show(); // want to change backto orginal photo
-  
-    $("#gem2").val(gem2).text(gem2);
-    $("#gem3").val(gem3).text(gem3);
-    $("#gem4").val(gem4).text(gem4);
-    
-    // $("#gem1").val(gem1).text(gem1); // want to change backto orginal photo
+    $("#ranBox").text("");
+    // $("#gem1").val(gem1).text(gem1); //   this?? 
     // $("#gem2").val(gem2).text(gem2);
     // $("#gem3").val(gem3).text(gem3);
     // $("#gem4").val(gem4).text(gem4);
-    
 }
 
 
-
-function checkScore(){
-    if (totalScore === ranNum){ 
+function checkScore() {
+    if (totalScore === ranNum) {
         win++;
-    $("#wins").html(win);
-    alert(" YOU WIN!! ")
-   // $(".GEMS").prop("disabled", );
-    reset();
-   
-}
-
-    else if (totalScore > ranNum) {
+        $("#wins").html(win);
+        alert(" YOU WIN!! ")
+        $("#wins").html(win);
+        reset();
+    } else if (totalScore > ranNum) {
         loss++;
-    $("#losses").html(loss);
-
-    alert("You LOOSE!");
-   // $(".GEMS").prop("disabled", true);
-    reset();
+        $("#losses").html(loss);
+        alert("You LOOSE!");
+        reset();
     }
 }
 
 // function getBtnValue(){
-//     var stringValue = $(this).val();
+//     var stringValue = $(this).val(); // this?? 
 //     return parseInt(stringValue);
 // }
 
@@ -97,52 +56,43 @@ $(document).ready(function () {
     $(".reset").click(function () {
         reset()
         $("#message").text("");
-       // $(".GEMS").prop("disabled", false);
     });
 
-
-    // $(".GEMS").on("click", function () {
+    // $(".GEMS").on("click", function () { \\this not working??? 
     //     var value = getBtnValue(this);
     //     totalScore += value;
-
     //     $("#score").text(totalScore);
-
     //     $(this).text(value);
-
     //     checkScore();
     // });
 
 });
-    $("#gem1").click(function () {
-        totalScore += gem1;
-        $('#score').text(totalScore);
-       $("#gem1").text(gem1);
-        checkScore();
-    })
+$("#gem1").click(function () {
+    totalScore += gem1;
+    $('#score').text(totalScore);
+    $("#ranBox").text(gem1);
+    checkScore();
+})
 
-    $("#gem2").click(function () {
-        totalScore += gem2;
-        $('#score').text(totalScore);
-        $("#gem2").text(gem2);
-        checkScore();
-    });
+$("#gem2").click(function () {
+    totalScore += gem2;
+    $('#score').text(totalScore);
+    $("#ranBox").text(gem2);
+    checkScore();
+});
 
-    $("#gem3").click(function () {
-        totalScore += gem3;
-        $("#score").text(totalScore);
-        $("#gem3").text(gem3);
-        checkScore();
-    });
+$("#gem3").click(function () {
+    totalScore += gem3;
+    $("#score").text(totalScore);
+    $("#ranBox").text(gem3);
+    checkScore();
+});
 
-    $("#gem4").click(function () {
-        totalScore += gem4;
-        $("#score").text(totalScore)
-        $("#gem4").text(gem4);
-        checkScore();
-    });
-
-    
-
-    
+$("#gem4").click(function () {
+    totalScore += gem4;
+    $("#score").text(totalScore)
+    $("#ranBox").text(gem4);
+    checkScore();
+});
 
 
